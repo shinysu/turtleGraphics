0a95e853472fd4de09d9420cbf9beaa2bb62ddc6
shinysu <shiny.suresh@gmail.com>
2019-12-02 21:32:37 +0530

    Incorporated comments raised in issue 'Suggestions for the next iteration(s) #1'

--- fibonacci.py
+++ fibonacci.py
@@ -1,55 +1,66 @@
-import turtle
-import math
+import turtle as t
+from math import pi
 
 
-def draw_square(a,b):
-    x.backward(a * scale)
-    x.right(90)
-    x.forward(b * scale)
-    x.left(90)
-    x.forward(b * scale)
-    x.left(90)
-    x.forward(b * scale)
+def draw_shape(scale_prev, scale_num):
+    '''draws a square with side scale_num. The fourth side of the square will be attached to the square for the previous
+    number in the sequence'''
+    x.backward(scale_prev)
+    x.right(angle)
+    for i in range(2):
+        x.forward(scale_num)
+        x.left(angle)
+    x.forward(scale_num)
 
 
-def draw_spiral(b):
-    circumference = math.pi * b * scale / 2
-    plot = circumference/90
-    for j in range(90):
+def draw_spiral(scale_num):
+    '''draws a half semicircle (quad) with the radius scale_num '''
+    circumference = pi * scale_num / 2
+    plot = circumference/angle
+    x._tracer(15)
+    for j in range(angle):
         x.forward(plot)
         x.left(1)
 
 
 def fibo_spiral():
+    '''draws a spiral; representing the fibonacci series'''
     x.penup()
     x.setposition(-scale, 0)
     x.seth(0)
     x.pendown()
     x.pencolor("red")
-
-    a=0
-    b=1
-    x.right(90)
+    prev = 0
+    num = 1
+    x.right(angle)
     for i in range(n):
-        draw_spiral(b)
-        a, b = b, a + b
+        scale_num = num * scale
+        draw_spiral(scale_num)
+        prev, num = num, prev + num
 
 
-def fibo_plot(n):
-    a = 0
-    b = 1
+def fibo_plot():
+    '''draws continuous squares with varied lengths that represents fibonacci series'''
+    prev = 0
+    num = 1
     x.pencolor("blue")
-    x.backward(b * scale)
+    x.backward(num * scale)
     for i in range(n):
-        draw_square(a, b)
-        a, b = b, a+b
+        scale_prev = prev * scale
+        scale_num = num * scale
+        draw_shape(scale_prev, scale_num)
+        prev, num = num, prev+num
 
 
 scale = 10
-n = int(input('Enter the number of iterations (must be > 0): '))
-x = turtle.Turtle()
-x.speed(1)
-if n > 1:
-    fibo_plot(n)
+angle = 90
+n = int(input('Enter the number of iterations (must be between 2 and 8): '))
+x = t.Turtle()
+x._tracer(1)
+if n > 1 and n < 9:
+    t.title("Visualization of Fibonacci series")
+    fibo_plot()
     fibo_spiral()
-turtle.done()
+else:
+    print("Please enter valid number of iterations (between 2 and 8")
+t.done()
